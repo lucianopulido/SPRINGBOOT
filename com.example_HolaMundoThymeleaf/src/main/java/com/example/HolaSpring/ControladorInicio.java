78f09ce77faffa579ca7690635c3f79813dec2d4
lucianopulido <lucianopulido1996@gmail.com>
2021-11-09 13:53:34 -0300

    PATRON MVC CON SPRINGBOOT

--- com.example_HolaMundoThymeleaf/src/main/java/com/example/HolaSpring/ControladorInicio.java
+++ com.example_HolaMundoThymeleaf/src/main/java/com/example/HolaSpring/ControladorInicio.java
@@ -1,16 +1,45 @@
 package com.example.HolaSpring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
-@RestController
+@Controller
 @Slf4j
 public class ControladorInicio {
 
+    @Value("${index.saludo}")
+    private String saludo;
+    
+    
     @GetMapping("/")
-    public String inicio(){
-        log.info("Ejecutando el controlador REST");
-        return "HolaMundo con Spring";
+    public String inicio(Model model){
+        String mensaje = "Mensaje con Thymeleaf";
+        
+        Persona persona = new Persona();
+        persona.setNombre("Luciano");
+        persona.setApellido("Pulido");
+        persona.setEmail("emaildeluciano@gmail.com");
+        persona.setTelefono("1155447788");
+        
+        Persona persona2 = new Persona();
+        persona2.setNombre("Jorge");
+        persona2.setApellido("Perez");
+        persona2.setEmail("emaildejorge@gmail.com");
+        persona2.setTelefono("1155447799");
+        
+        ArrayList personas = new ArrayList();
+        personas.add(persona);
+        personas.add(persona2);
+        
+        log.info("Ejecutando el controlador spring MVC");
+        model.addAttribute("mensaje",mensaje);
+        model.addAttribute("saludo",saludo);
+      //  model.addAttribute("persona",persona);
+        model.addAttribute("personas",personas);
+        return "index";
     }
 }
---
+++ com.example_HolaMundoThymeleaf/src/main/java/com/example/HolaSpring/Persona.java
@@ -0,0 +1,12 @@
+package com.example.HolaSpring;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
